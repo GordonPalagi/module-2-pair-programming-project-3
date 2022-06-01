2a4daf316b20f319c1870f95b9a528f096435843
Ishwor Tuladhar <ishwor.tuladhar@gmail.com>
2022-05-31 21:55:06 -0400

    PairProgramming-3 Submission Complete.

--- src/main/java/com/techelevator/services/RestCatFactService.java
+++ src/main/java/com/techelevator/services/RestCatFactService.java
@@ -3,6 +3,7 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatFact;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
@@ -10,7 +11,10 @@ public class RestCatFactService implements CatFactService {
 	@Override
 	public CatFact getFact() {
 		// TODO Auto-generated method stub
-		return null;
+		RestTemplate restTemplate = new RestTemplate();
+		String url="https://cat-data.netlify.app/api/facts/random";
+		CatFact fact = restTemplate.getForObject(url, CatFact.class);
+		return fact;
 	}
 
 }
